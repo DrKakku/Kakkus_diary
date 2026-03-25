a94924f86681cdff8a34711b663b36684a1189bd
DrKakku <DrKakku19@gmail.com>
2026-03-25 21:55:18 +0530

    ungdu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://drkakku.github.io',
+  base: '/Kakkus_diary', // IMPORTANT: repo name
+  output: 'static'
+});
